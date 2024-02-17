3c790251ba5f1f2c22a8217fcd3c977eb6aa0995
Linh Nguyen <antony.nguyen0901@gmail.com>
2024-02-17 15:54:16 +0700

    change config

--- config.ts
+++ config.ts
@@ -1,8 +1,8 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
+  url: "https://www.shopeeanalytics.com",
+  match: "https://www.shopeeanalytics.com/vn/ma-giam-gia/**",
   maxPagesToCrawl: 50,
   outputFileName: "output.json",
 };
